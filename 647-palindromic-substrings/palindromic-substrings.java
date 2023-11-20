bcb09191995a2910a55596630de3840e4866d7e0
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-20 14:08:30 +0530

    Time: 2 ms (95.58%) | Memory: 40.8 MB (45.84%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int expandAroundIndex(String s,int i,int j)
+    {
+        int count = 0;
+
+        while(i>=0 && j<s.length() && s.charAt(i) == s.charAt(j))
+        {
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    public int countSubstrings(String s) {
+        int count = 0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            int oddKaAns = expandAroundIndex(s,i,i);
+            count = count + oddKaAns;
+
+            int evenKaAns = expandAroundIndex(s,i,i+1);
+            count = count + evenKaAns;
+        }    
+
+        return count;
+    }
+}
