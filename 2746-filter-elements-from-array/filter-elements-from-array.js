9cadaafc5ec0063951af81ea5b240d1471deffe8
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-20 14:00:58 +0530

    Time: 53 ms (44.02%) | Memory: 41.8 MB (67.73%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const result = [];
+    arr.forEach((value,index)=>
+    {
+        if(fn(value,index))
+        {
+            result.push(value);
+        }
+    });
+    return result;
+};
