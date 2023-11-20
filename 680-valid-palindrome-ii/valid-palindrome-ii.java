7b0760d0d0444d81059fdfcf76be49fc9b4885d5
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-20 14:08:41 +0530

    Time: 5 ms (85.73%) | Memory: 44.1 MB (71.20%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    static boolean checkPalindrome(String s,int i,int j)
+    {
+        while(i<=j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+   public boolean validPalindrome(String s) {
+    int i=0;
+    int j=s.length()-1;
+
+    while(i<=j)
+    {
+        if(s.charAt(i)!=s.charAt(j))
+        {
+            return checkPalindrome(s,i+1,j) || checkPalindrome(s,i,j-1);
+        }
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    return true;
+    }
+}
