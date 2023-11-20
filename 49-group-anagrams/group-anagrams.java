4c849d5270ca1f2fd59c07904599a7457f397480
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-20 14:07:31 +0530

    Time: 6 ms (73.97%) | Memory: 46.7 MB (71.76%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        HashMap <String,List<String>> map = new HashMap<>();
+
+        for(String word:strs)
+        {
+            char []arr = word.toCharArray();
+
+            Arrays.sort(arr);
+
+            String sortedWord = new String(arr);
+
+            if(!map.containsKey(sortedWord))
+            {
+                map.put(sortedWord,new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
