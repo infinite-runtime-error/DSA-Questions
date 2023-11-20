7898a3b7a6d125362bd933984f026d3a4f10d732
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-20 14:07:50 +0530

    Time: 5 ms (54.82%) | Memory: 42 MB (80.77%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        // Method 1:- Easy Method(Brute Force)
+        // char []arr1 = s.toCharArray();
+        // char []arr2 = t.toCharArray();
+
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
+
+        // return Arrays.equals(arr1,arr2);
+
+        // Method 2:- Using Frequency Table
+        int []frequencyTable = new int[256];
+
+        for(int i=0;i<s.length();i++)
+        {
+            frequencyTable[s.charAt(i)]++;
+            frequencyTable[t.charAt(i)]--;
+        }
+
+        for(int i=0;i<256;i++)
+        {
+            if(frequencyTable[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
