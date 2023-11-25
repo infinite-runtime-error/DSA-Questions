8ae84425ced7eb3d91b06ccff90047ba0bf564dd
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-25 15:00:04 +0530

    Time: 1 ms (81.19%) | Memory: 40.5 MB (41.18%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -25,6 +25,7 @@ class Solution {
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
         int index = 0;
+
         while(index<s1.length() && index<s2.length())
         {
             if(s1.charAt(index)==s2.charAt(index))
@@ -35,7 +36,8 @@ class Solution {
             {
                 break;
             }
+            
         }
-        return s1.substring(0,index);
+        return s2.substring(0,index);
     }
 }
