3d7b98b0e8186e70109ff2c422a1bb71669daccd
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-25 14:17:22 +0530

    Time: 7 ms (70.57%) | Memory: 43.4 MB (91.05%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,37 +1,32 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        
-       // Step 1:- Create an Empty Array
-       int [] empty = new int [timePoints.size()];
+        // Step 1:-
+        int []arr = new int [timePoints.size()];
 
-       // Step 2:- Convert String into Int and store in arrays
-       for(int i=0;i<timePoints.size();i++)
-       {
-           String currentString = timePoints.get(i);
-           int hours = Integer.parseInt(currentString.substring(0,2));
-           int minutes = Integer.parseInt(currentString.substring(3,5));
-           int totalMinutes = (hours*60)+minutes;
-           empty[i] = totalMinutes;
-       }
+        // Step 2:-
+        for(int i=0;i<timePoints.size();i++)
+        {
+            String current = timePoints.get(i);
+            int hours = Integer.parseInt(current.substring(0,2));
+            int minutes = Integer.parseInt(current.substring(3,5));
+            int totalMinutes = ( hours * 60 )+minutes;
+            arr[i] = totalMinutes;
+        }
 
-       // Step 3:- Sort Array
-       Arrays.sort(empty);
+        // Step 3:-
+        Arrays.sort(arr);
 
-       // Step 4:- Find Minimum Difference between elements of empty array
-       int mini = Integer.MAX_VALUE;
-       for(int i=0;i<empty.length-1;i++)
-       {
-           int difference = empty[i+1]-empty[i];
-           mini = Math.min(mini,difference);
-       }
+        // Step 4:- 
+        int mini = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int miniDifference = arr[i+1]-arr[i];
+            mini = Math.min(mini,miniDifference);
+        }
 
-       // Step 5:- Find minimum element between mini and lastDifference
-       int lastDifference = (empty[0]+1440)-empty[empty.length-1];
-       mini = Math.min(mini,lastDifference);
+        int lastDifference = (arr[0]+1440) - (arr[arr.length-1]);
+        mini = Math.min(mini,lastDifference);
 
-       return mini;
-
-
-
+        return mini;
     }
 }
