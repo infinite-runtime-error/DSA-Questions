e7e8835eb913d8e1f698a2d81b40dab0a810c917
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-25 14:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (95.05%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.java
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -2,12 +2,13 @@ class Solution {
     public String reverseOnlyLetters(String s) {
       
         // 2 Pointer Approach
-        char [] arr = s.toCharArray();
+        char []arr = s.toCharArray();
         int start = 0;
-        int end = arr.length-1;
+        int end = s.length()-1;
 
         while(start<=end)
         {
+
             if(!Character.isLetter(arr[start]))
             {
                 start++;
@@ -18,13 +19,14 @@ class Solution {
             }
             else
             {
-                // Swap
                 char temp = arr[start];
                 arr[start] = arr[end];
                 arr[end] = temp;
                 start++;
                 end--;
             }
+
+            
         }
         return String.valueOf(arr);
     }
