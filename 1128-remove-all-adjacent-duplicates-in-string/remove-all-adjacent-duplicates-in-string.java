ce4ba79234469b2743d0a1a359d68058c83788db
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-25 13:33:35 +0530

    Time: 46 ms (32.20%) | Memory: 44 MB (88.72%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,38 +1,32 @@
 class Solution {
     public String removeDuplicates(String s) {
+        // Creating A StringBuilder , because it is only mutable String in Java
+        StringBuilder sb = new StringBuilder(s);
+        int i = 0;
+        int j = 1;
+        while(j<sb.length())
+        {
+            if(sb.charAt(i)==sb.charAt(j))
+            {
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
 
-            if(s.length()<=1)
-     {
-         return s;
-     }
+                if(i!=0)
+                {
+                    i--;
+                }
+                if(j!=1)
+                {
+                    j--;
+                }
 
-     StringBuilder sb = new StringBuilder(s);
-     int i = 0;
-     int j = 1;
-
-     while(j<sb.length())
-     {
-         if(sb.charAt(i)==sb.charAt(j))
-         {
-             sb.deleteCharAt(i);
-             sb.deleteCharAt(i);
-
-             if(i != 0)
-             {
-                 i--;
-             }
-             if(j != 1)
-             {
-                 j--;
-             }
-         }
-         else
-         {
-             i++;
-             j++;
-         }
-
-     }
-     return sb.toString();
+            }
+            else 
+            {
+                i++;
+                j++;
+            }
+        }
+        return sb.toString();
     }
 }
