e1b25a568792e22b8e562392085bd007f07f77ec
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-11-25 15:24:07 +0530

    Time: 2 ms (45.13%) | Memory: 41.6 MB (23.40%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -5,35 +5,34 @@ class Solution {
       // from starting and ending
 
       s = s.strip();
-      if(s.length() == 0)
+      if(s.length()==0)
       {
           return 0;
       }
 
-      int sign = s.charAt(0) == '-'?-1:1;
-      if(s.charAt(0) == '-' || s.charAt(0) == '+')
-      {
-          s = s.substring(1);
-      }
 
-      long num = 0;
-      for(char c : s.toCharArray())
-      {
+        int sign = s.charAt(0) == '-'?-1:1;
+        if(s.charAt(0) == '-' || s.charAt(0) == '+')
+        {
+            s = s.substring(1);
+        }
+        long num = 0;
+        for(char c:s.toCharArray())
+        {
             if(!Character.isDigit(c))
             {
                 break;
             }
             else
             {
-                num = num * 10 + (c - '0');
+                num = num * 10 + (c -'0');
             }
             if (sign * num <= Integer.MIN_VALUE)
             return Integer.MIN_VALUE;
 
             if (sign * num >= Integer.MAX_VALUE)
             return Integer.MAX_VALUE;
-
-      }
+        }
 
       return sign*(int)num;
   }
