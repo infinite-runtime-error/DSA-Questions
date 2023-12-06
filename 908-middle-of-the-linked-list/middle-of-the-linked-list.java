6c72059d3393de7aa0bca4381e3de7f5edbb0f7b
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-12-06 11:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (5.09%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        // Simple Approach
+        ListNode temp = head;
+        int length = 0;
+        while(temp!=null)
+        {
+            temp = temp.next;
+            length++;
+        }
+
+        int half = length/2;
+        temp = head;
+        while(half-- > 0)
+        {
+            temp = temp.next;
+        }
+        return temp;
+
+
+        
+    }
+}
