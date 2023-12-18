6e8cb5f2fa6da8eededec33e4a0b965eb2f7d23c
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-12-18 16:22:23 +0530

    Time: 2 ms (95.69%) | Memory: 40.7 MB (59.67%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,27 +1,25 @@
 class Solution {
-    static int expandAroundIndex(String s,int i,int j)
-    {
-        int count = 0;
-        while(i>=0 && j<s.length() && s.charAt(i) == s.charAt(j))
-        {
-            count++;
-            i--;
-            j++;
-        }
-        return count;
-    }
+   static int expandAroundIndex(String s,int start,int end)
+   {
+       int count = 0;
+       while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end))
+       {
+           count++;
+           start--;
+           end++;
+       }
+       return count;
+   }
     public int countSubstrings(String s) {
         int count = 0;
-
         for(int i=0;i<s.length();i++)
         {
             int oddKaAns = expandAroundIndex(s,i,i);
-             count += oddKaAns;
+            count += oddKaAns;
 
             int evenKaAns = expandAroundIndex(s,i,i+1);
-             count += evenKaAns;
-        }        
-
+            count += evenKaAns;
+        }
         return count;
     }
 }
