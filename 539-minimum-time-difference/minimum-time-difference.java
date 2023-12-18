6e4e9ce83959e0fa0ef003a3715144b5a6371dc2
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2023-12-18 16:15:13 +0530

    Time: 5 ms (70.23%) | Memory: 44.6 MB (32.74%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,30 +1,31 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        // Step 1:-
-        int []arr = new int [timePoints.size()];
+        // Step 1:- Create an Empty Array of same size as of timePoints
+        int []arr = new int[timePoints.size()];
 
-        // Step 2:-
+        // Step 2:- Traverse String Array
         for(int i=0;i<timePoints.size();i++)
         {
             String current = timePoints.get(i);
             int hours = Integer.parseInt(current.substring(0,2));
             int minutes = Integer.parseInt(current.substring(3,5));
-            int totalMinutes = ( hours * 60 )+minutes;
+            int totalMinutes = hours*60 + minutes;
             arr[i] = totalMinutes;
         }
 
-        // Step 3:-
+        // Step 3:- Sort Array
         Arrays.sort(arr);
 
-        // Step 4:- 
+        // Step 4:- Find Minimum Time Differnce Between all elements
         int mini = Integer.MAX_VALUE;
         for(int i=0;i<arr.length-1;i++)
         {
-            int miniDifference = arr[i+1]-arr[i];
-            mini = Math.min(mini,miniDifference);
+            int miniTime = arr[i+1]-arr[i];
+            mini = Math.min(miniTime,mini);
         }
 
-        int lastDifference = (arr[0]+1440) - (arr[arr.length-1]);
+        // Step 5:- Comparing Last Minimum Difference
+        int lastDifference = ((arr[0]+1440)-arr[arr.length-1]);
         mini = Math.min(mini,lastDifference);
 
         return mini;
