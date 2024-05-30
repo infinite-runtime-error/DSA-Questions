f06e317f1a8f3e4e40e23f33c015a1f4a09fd51a
Jatin Rana <infiniteruntimeerror@gmail.com>
2024-05-30 15:38:33 +0530

    Time: 21 ms (32.97%) | Memory: 35.2 MB (17.96%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -23,7 +23,30 @@ public:
         return -1;
     }
 
+    int prefixSumApproach(vector<int>& nums){
+        vector<int> leftSum(nums.size(),0);
+        vector<int> rightSum(nums.size(),0);
+
+        // calculate left sum and store in leftSum Vector
+        for(int i=1;i<nums.size();i++){
+            leftSum[i] = leftSum[i-1] + nums[i-1];
+        }
+
+        // calculate right sum and store in rightSum Vector
+        for(int j=nums.size()-2;j>=0;j--){
+            rightSum[j] = rightSum[j+1] + nums[j+1];
+        }
+
+        // check if there exists an index at which leftSum[index] == rightSum[index]
+        for(int i=0;i<nums.size();i++){
+            if(leftSum[i] == rightSum[i]) return i;
+        }
+        return -1;
+    }
+
     int pivotIndex(vector<int>& nums) {
-        return bruteForce(nums);
+        // return bruteForce(nums);
+
+        return prefixSumApproach(nums);
     }
 };
