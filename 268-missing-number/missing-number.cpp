7114be23bfc6c4d9d5533dc057e56d2a25d53f2d
Jatin Rana <infiniteruntimeerror@gmail.com>
2024-05-30 16:43:13 +0530

    Time: 12 ms (68.19%) | Memory: 21.1 MB (13.78%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    int sortingMethod(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++){
+            if(i == nums[i]){
+                continue;
+            }
+            else {
+                return i;
+            }
+        }
+        return nums.size();
+    }
+
+    int missingNumber(vector<int>& nums) {
+        return sortingMethod(nums);
+    }
+};
